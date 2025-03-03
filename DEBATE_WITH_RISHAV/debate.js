e6f711c523a27f8ea6c711d6bc73bc846403ff51
RiKa1996 <rishu2041996@gmail.com>
2025-03-03 17:35:11 +0530

    updated done

--- DEBATE_WITH_RISHAV/debate.js
+++ DEBATE_WITH_RISHAV/debate.js
@@ -1,6 +1,9 @@
 const heroSection = document.getElementById('heroSection');
 const overlayCon = document.getElementById('overlayCon');
 const overlayBtn = document.getElementById('overlayBtn');
+const copy = document.querySelector(".topper-debaters").cloneNode(true);    /* This is topper section, cloneNode-append child krne ke liye */
+const API_KEY = "c52d9d65029049abaa654c0df3f6a7fc";    /* this is live news section */
+const url = "https://newsapi.org/v2/everything?q=";
 
 overlayBtn.addEventListener('click', () => {
     heroSection.classList.toggle('right-panel-active');
@@ -10,3 +13,47 @@ overlayBtn.addEventListener('click', () => {
         overlayBtn.classList.add ('btnScaled')
     })
 });
+document.querySelector(".debaters").appendChild(copy);
+
+
+
+/* ====================news section======================================== */
+window.addEventListener("load", ()=> fetchNews("India"));
+
+async function fetchNews (query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    /* console.log(data); */
+    bindData(data.articles);       /* bahot se article ko na le sake liye kuch hi article lene ke liye binddata kiya gya hai */
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById('cards-container');
+    const newsCardTemplate = document.getElementById('template-news-card');
+
+    cardsContainer.innerHTML = "";
+    
+    articles.forEach(article => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true); /* iska mtlb hai ki hum sare div ko lena chahte hai */
+        fillDataInCard(cardClone, article);             /* iska hum ek function seprate bnayage tati sari image , title aur bhi details la sake */
+        cardsContainer.appendChild(cardClone);
+    });
+}
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+    const date = new Date(article.publishedAt).toLocaleDateString("en-US", {
+        timeZone: "Asia/Jakarta"
+    });
+    newsSource.innerHTML = `${article.source.name} ⋅ ${date}`;
+
+
+
+}
